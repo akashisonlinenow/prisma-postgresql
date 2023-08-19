c47d1e8d9c0ba2fe3254ceb6c81e0cb8b8a838cc
Akash Shridharan <akashshridharan2802@gmail.com>
2023-08-19 21:00:22 +0530

    last commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import Image from 'next/image'
 import Link from 'next/link'
 
-export default function Home() {
+async function getPosts(){
+  const res = await fetch(`${process.env.BASE_URL}/api/getPosts`)
+  if(!res.ok){
+    console.log(res)
+  }
+  return res.json()
+}
+
+export default async function Home() {
+  const data: {id: number; title: string }[] = await getPosts()
+  console.log(data)
   return (
     <main className='py-8 px-48'>
       <Link href={"/dashboard"} className='bg-teal-700 text-black font-medium py-2 px-4 rounded-md'> Go to dashboard</Link>
+      {data.map((post) => (
+        <h1 className='text-lg py-6'>{post.title}</h1>
+      ))}
     </main>
   )
 }
