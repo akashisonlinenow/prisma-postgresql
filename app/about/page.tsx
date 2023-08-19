a621eabb21e05c6371a8120998821849244ede72
Akash Shridharan <akashshridharan2802@gmail.com>
2023-08-19 19:30:51 +0530

    created next app

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 
-export default function Home() {
+export default function About() {
   return (
     <main>
-      <h1>Home page</h1>
+      <h1>About page</h1>
     </main>
   )
 }
